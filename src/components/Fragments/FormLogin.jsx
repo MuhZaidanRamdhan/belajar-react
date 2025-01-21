e3eaa6bb7aa5842a868ba8e13c90abefe8b96706
MuhZaidanRamdhan <muhammadzaidanramdhan509@gmail.com>
2025-01-21 11:24:52 +0700

    event handler using localstorage

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import InputForm from '../Elements/Input'
-import Button from '../Elements/Button'
+import React from "react";
+import InputForm from "../Elements/Input";
+import Button from "../Elements/Button";
 
 function FormLogin() {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    localStorage.setItem("email", e.target.email.value);
+    localStorage.setItem("password", e.target.password.value);
+    window.location.href = "/products";
+  };
   return (
-    <form action="">
-    <InputForm
-      label="Email"
-      type="email"
-      placeholder="example@example.com"
-      name="email"
-    />
-    <InputForm
-      label="Password"
-      type="password"
-      placeholder="*********"
-      name="password"
-    />
-    <Button classname="bg-blue-600 w-full">Login</Button>
-  </form>
-  )
+    <form onSubmit={handleLogin}>
+      <InputForm
+        label="Email"
+        type="email"
+        placeholder="example@example.com"
+        name="email"
+      />
+      <InputForm
+        label="Password"
+        type="password"
+        placeholder="*********"
+        name="password"
+      />
+      <Button classname="bg-blue-600 w-full" type="submit">
+        Login
+      </Button>
+    </form>
+  );
 }
 
-export default FormLogin
+export default FormLogin;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,6 @@
+import { Fragment } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
+import Button from "../components/Elements/Button";
 
 const products = [
   {
@@ -24,22 +26,34 @@ const products = [
     image: "/images/shoes-1.jpg",
     description:
       "lorem ipsum dolor sit amet, consectet, sed diam nonumy eirmod tempor, sed diam nonumy eirmod, tempor, sed diam nonumy eirmod",
-  }
+  },
 ];
 
+const email = localStorage.getItem("email");
+
 const ProductsPage = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
   return (
-    <div className="flex justify-center py-5">
-      {products.map((product) => (
-        <CardProduct>
-          <CardProduct.Header image={product.image}></CardProduct.Header>
-          <CardProduct.Body name={product.name}>
-            {product.description}
-          </CardProduct.Body>
-          <CardProduct.Footer price={product.price}></CardProduct.Footer>
-        </CardProduct>
-      ))}
-    </div>
+    <Fragment>
+      <div className="flex justify-end h-20 px-10 bg-blue-600 text-white items-center">
+        {email} <Button classname="bg-red-600 ml-5" onClick={handleLogout}>Logout</Button>
+      </div>
+      <div className="flex justify-center py-5" >
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image}></CardProduct.Header>
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price}></CardProduct.Footer>
+          </CardProduct>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
